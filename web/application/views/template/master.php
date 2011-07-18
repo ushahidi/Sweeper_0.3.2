0b33bdf0c8c1d7024286373bd5e26312400dc251
Charl van Niekerk <charl@ushahidi.com>
2011-07-18 20:58:58 +0200

    Removed old tracking.

--- web/application/views/template/master.php
+++ web/application/views/template/master.php
@@ -93,17 +93,5 @@
                 </div>
             </div>
         </div>
-        <!-- Begin SwiftRiver Tracking -->
-        <script type="text/javascript">
-            var pkBaseURL = (("https:" == document.location.protocol) ? "https://swift.ushahidi.com/stats/" : "http://swift.ushahidi.com/stats/");
-            document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-            </script><script type="text/javascript">
-            try {
-            var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
-            piwikTracker.trackPageView();
-            piwikTracker.enableLinkTracking();
-            } catch( err ) {}
-        </script>
-        <noscript><p><img src="http://swift.ushahidi.com/stats/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
     </body>
 </html>
