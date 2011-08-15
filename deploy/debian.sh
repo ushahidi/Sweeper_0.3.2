627e1833183ced8b9f6417fc96f3da880a4bc429
Charl van Niekerk <charlvn@charlvn.com>
2011-08-15 10:08:04 +0300

    Added Debian 6.0 deployment script.

---
+++ deploy/debian.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Perform Debian updates.
+apt-get update
+apt-get upgrade -y
+
+# Install necessary Debian packages.
+apt-get install -y apache2 mysql-server php5 php5-curl php5-mysql php-pear git-core
+
+# Enable Apache mod_rewrite.
+a2enmod rewrite
+
+# Enable Apache .htaccess files.
+sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/sites-enabled/000-default
+
+# Create MySQL user account for Sweeper.
+echo 'create database sweeper default charset utf8; grant all on sweeper.* to sweeper@localhost identified by "sweeper";' | mysql -u root -p
+
+# Install the PHP PEAR Log package.
+pear install Log
+
+# Restart Apache to ensure new configuration is loaded.
+/etc/init.d/apache2 restart
+
+# Clone Sweeper and Swiftriver repositories into /var/www.
+cd /var/www
+rm index.html
+git clone https://github.com/ushahidi/Sweeper.git .
+git clone https://github.com/ushahidi/Swiftriver.git core
+
+# Transfer ownership of the application to the same user as the Apache process.
+chown -R www-data:www-data .
